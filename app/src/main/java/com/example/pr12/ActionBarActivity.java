6bdf59ba257ef8485f14d6e564287d2dd72f5a8d
Smorodnikova <smorodnikova@iMac-Administrator.local>
2024-02-07 11:52:10 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pr12/ActionBarActivity.java
@@ -0,0 +1,4 @@
+package com.example.pr12;
+
+public class ActionBarActivity {
+}
---
+++ app/src/main/java/com/example/pr12/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.pr12;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private Button login;
+    private TextView loginLocked;
+    private TextView attempts;
+    private TextView numberOfAttempts;
+
+    int numberOfRemainLoginAttempts = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Login(set);
+    }
+    public void Login(View view) {
+        if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+            Toast.makeText(getApplicationContext(), "Вход выполнен!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, Second.class);
+        } else {
+            Toast.makeText(getApplicationContext(), "Неправильные данные!", Toast.LENGTH_SHORT).show();
+            int numberOfRemainingLoginAttempts = 3;
+            numberOfRemainingLoginAttempts--;
+            attempts.setVisibility(View.VISIBLE);
+            numberOfAttempts.setVisibility(View.VISIBLE);
+            numberOfAttempts.setText(Integer.toString(numberOfRemainingLoginAttempts));
+            if (numberOfRemainingLoginAttempts == 0) {
+                login.setEnabled(false);
+                loginLocked.setVisibility(View.VISIBLE);
+                loginLocked.setBackgroundColor(Color.RED);
+                loginLocked.setText("Вход заблокирован!!!");
+            }
+        }
+    }
+}
+
